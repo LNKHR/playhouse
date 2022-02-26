52291d2a1bc72022b98d9558ce9d54c44baa5c9b
sylueon <jammydcreates@gmail.com>
2022-02-26 19:07:16 +1100

    removed the regex too risky for now

--- template_creator/creator.js
+++ template_creator/creator.js
@@ -269,9 +269,10 @@ function insertInput() {
 
         var inputChangeTest = inputChangeTest.replaceAll(`{{${bigArray[i].itemList.replaceAll('-', ' ')}}}`, `{{${bigArray[i].itemInput}:${bigArray[i].itemTitle}|${inputValue}}}`);
 
-        var regex = new RegExp(`{{${bigArray[i].itemInput}:${bigArray[i].itemID.replaceAll('-',' ')}`+'.*}}', "gmi");
+        // var regex = new RegExp(`{{${bigArray[i].itemInput}:${bigArray[i].itemID.replaceAll('-',' ')}`+'.*}}', "gmi");
+        // var inputChange = inputChange.replaceAll(regex, `${bigArray[i].userInput}`).replace(/{{section(?:.+)}}/gm, "").replace(/{{subsection(?:.+)}}/gm, "");
 
-        var inputChange = inputChange.replaceAll(regex, `${bigArray[i].userInput}`).replace(/{{section(?:.+)}}/gm, "").replace(/{{subsection(?:.+)}}/gm, "");
+        var inputChange = inputChange.replaceAll(`{{${bigArray[i].itemList}}}`, `${bigArray[i].userInput}`).replace(/{{section(?:.+)}}/gm, "").replace(/{{subsection(?:.+)}}/gm, "").replaceAll(`{{${bigArray[i].itemInput}:${bigArray[i].itemID.replaceAll('-',' ')}}}`, `${bigArray[i].userInput}`);
 
 
         code.innerHTML = inputChange;
